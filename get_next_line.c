63549f340e140efebbb3287dd523ed53067e3133
mouiyada <osaraman@hotmail.com>
2025-02-16 15:03:29 +0900

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kyamada <kyamada@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 15:19:56 by kyamada           #+#    #+#             */
-/*   Updated: 2025/02/11 16:33:46 by kyamada          ###   ########.fr       */
+/*   Updated: 2025/02/16 14:52:05 by kyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,47 +29,82 @@ int	ft_search_next(char *bufp)
 char	*ft_read_and_join(int fd, char *bufp)
 {
 	char	buf[BUFSIZ];
-	size_t	count;
+	ssize_t	count;
 	char	*tmp;
 
 	while (1)
 	{
+		if (ft_search_next(bufp))
+			break ;
 		count = read(fd, buf, BUFSIZ);
-		if (count <= 0)
+		if (count < 0)
+		{
+			free(bufp);
+			return (NULL);
+		}
+		if (count == 0)
 			break ;
 		buf[count] = '\0';
 		tmp = ft_strjoin(bufp, buf);
-//		if (bufp)
-//			free(bufp);
+		free(bufp);
+		if (!tmp)
+			return (NULL);
 		bufp = tmp;
-		if (ft_search_next(bufp))
-			break ;
 	}
 	return (bufp);
 }
 
-char	*get_next_line(int fd)
+char	*ft_extract_line(char **str)
 {
-	static char	*str = "";
-	char		*line;
-	size_t		i;
-	char		*substr;
+	char	*line;
+	char	*new_str;
+	size_t	i;
 
-	if (fd < 0 || BUFSIZ <= 0)
-		return (NULL);
-	str = ft_read_and_join(fd, str);
-	if (!str || str[0] == '\0')
-		return (NULL);
 	i = 0;
-	while (str[i] != '\0' && str[i] != '\n')
+	while ((*str)[i] != '\0' && (*str)[i] != '\n')
 		i++;
-	line = ft_substr(str, 0, i + 1);
-	substr = ft_substr(str, i + 1, ft_strlen(str) - i - 1);
-	free(str);
-	str = substr;
+	line = ft_substr(*str, 0, i + 1);
+	if (!line)
+		return (NULL);
+	new_str = ft_substr(*str, i + 1, ft_strlen(*str) - i - 1);
+	free(*str);
+	*str = new_str;
+	if (!new_str)
+	{
+		free(line);
+		return (NULL);
+	}
 	return (line);
 }
 
+char	*get_next_line(int fd)
+{
+	static char	*str = NULL;
+	char		*line;
+
+	if (fd < 0 || BUFSIZ <= 0)
+		return (NULL);
+	if (!str)
+	{
+		str = ft_strdup("");
+		if (!str)
+			return (NULL);
+	}
+	str = ft_read_and_join(fd, str);
+	if (!str || str[0] == '\0')
+	{
+		free(str);
+		str = NULL;
+		return (NULL);
+	}
+	line = ft_extract_line(&str);
+	if (!line)
+	{
+		free(str);
+		str = NULL;
+	}
+	return (line);
+}
 // int main(void) {
 //     int fd;
 
@@ -79,11 +114,11 @@ char	*get_next_line(int fd)
 //         perror("Failed to open file");
 //         return (1);
 //     }
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
+// 	printf("%s\n", get_next_line(fd));
+// 	printf("%s\n", get_next_line(fd));
+// 	printf("%s\n", get_next_line(fd));
+// 	printf("%s\n", get_next_line(fd));
+// 	printf("%s\n", get_next_line(fd));
 // 	close(fd);
 // 	return 0;
 // }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,12 @@
 /*   By: kyamada <kyamada@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 15:18:39 by kyamada           #+#    #+#             */
-/*   Updated: 2025/02/11 15:53:48 by kyamada          ###   ########.fr       */
+/*   Updated: 2025/02/16 14:29:52 by kyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-//# define BUFFER_SIZE 10
 
 # include <unistd.h>
 # include <fcntl.h>
@@ -21,13 +19,14 @@
 # include <stdlib.h>
 # include <stdint.h>
 
-int					ft_search_next(char *bufp);
-char				*ft_read_and_join(int fd,char *bufp);
 char				*get_next_line(int fd);
 size_t				ft_strlen(const char *str);
 void				*ft_memcpy(void *dest, const void *src, size_t n);
 char				*ft_strdup(const char *s);
 char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 0
 
+# endif
 #endif
